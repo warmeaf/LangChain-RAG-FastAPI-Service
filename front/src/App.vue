ebe2612db40fba7e340a512dc3bddcbeb601883d
warmleaf <1391040917@qq.com>
2026-06-20 02:48:51 +0800

    style(front/src/App.vue): 移除全局样式和app类名封装
    将原有的全局重置样式和app容器样式移除，简化App.vue的结构，把全局样式迁移至全局css配置中，统一管理页面基础样式

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <van-config-provider :theme="themeStore.currentTheme === 'dark' ? 'dark' : ''">
-    <div class="app">
+    <div>
       <router-view v-slot="{ Component }">
         <template v-if="$route.meta.keepAlive">
           <keep-alive>
@@ -22,30 +22,3 @@ import { useThemeStore } from './store/theme'
 const themeStore = useThemeStore()
 onMounted(() => themeStore.initTheme())
 </script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-html, body {
-  font-size: var(--van-font-size-lg);
-  height: 100%;
-  width: 100%;
-}
-
-.app {
-  max-width: 750px;
-  margin: 0 auto;
-  height: 100%;
-}
-
-/* 移动端适配 */
-@media screen and (max-width: 750px) {
-  html {
-    font-size: calc(100vw / 750 * 16); /* rem 基准，16px = var(--van-font-size-lg) */
-  }
-}
-</style>
