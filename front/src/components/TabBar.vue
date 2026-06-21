8e622a09ebffa989bfc8274d737ea976fc8cf53d
warmleaf <1391040917@qq.com>
2026-06-21 17:00:54 +0800

    fix: '我的'Tab图标从 User 改为 UserRound

--- front/src/components/TabBar.vue
+++ front/src/components/TabBar.vue
@@ -14,7 +14,7 @@
     </van-tabbar-item>
     <van-tabbar-item name="my" @click="router.push('/my')">
       <template #icon>
-        <User :size="18" />
+        <UserRound :size="18" />
       </template>
       <span>{{ $t('nav.my') }}</span>
     </van-tabbar-item>
@@ -22,7 +22,7 @@
 </template>
 
 <script setup lang="ts">
-import { Book, MessageCircle, User } from '@lucide/vue'
+import { Book, MessageCircle, UserRound } from '@lucide/vue'
 import { ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
